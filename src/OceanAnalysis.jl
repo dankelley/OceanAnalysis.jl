ffe73d66b0aaffe4a761e9a6655387dc8d923c05
dankelley <kelley.dan@gmail.com>
2025-08-16 15:21:23 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -398,7 +398,7 @@ function getElement(o::Ctd, name::String; debug::Bool=false)
         println("in getElement([Ctd object], name=$name")
     end
     # Handle directly-stored items
-    if name in fieldnames(o)
+    if name in fieldnames(Ctd)
         println("DAN")
         return copy(getproperty(o, Symbol(name)))
     end
