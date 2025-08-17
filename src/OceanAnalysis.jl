dd20d8da324888b7ccb341eda55b2f77e92b13db
dankelley <kelley.dan@gmail.com>
2025-08-17 09:52:26 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -466,12 +466,12 @@ function N2(o::Ctd, s::Float64=1.0; debug::Bool=false)
         println(j)
         println("length(i)=", length(i), ", length(j)=", length(j))
     end
-    local spline = Spline1D(pressure[j], sigma0[j], w=ones(sum(ok)), k=3, bc="zero", s=s)
+    local spline = Spline1D(pressure[j], sigma0[j], w=ones(sum(ok)), k=3, bc="nearest", s=s)
     sigma0p = evaluate(spline, pressure)
     rho0 = 1000.0 + mean(sigma0p)
     g = 9.8
     deriv = diff(sigma0p) ./ diff(pressure)
-    deriv = [deriv[1]; deriv]
+    deriv = [deriv; deriv[end]]
     if debug
         println("deriv:")
         print(deriv)
