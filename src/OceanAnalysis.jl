ebccf9165d63d60fb9b238944706b2b81d820bde
dankelley <kelley.dan@gmail.com>
2025-08-23 14:18:37 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -94,8 +94,9 @@ function as_Ctd(salinity::Vector{Float64}, temperature::Vector{Float64}, pressur
     longitude::Float64=-30.0, latitude::Float64=30.0;
     debug::Int64=0)
     ds = debug_space(debug)
+    println("ds=$ds")
     if debug > 0
-        println("$ds as_Ctd() START")
+        println("$ds as_Ctd(<ctd>, debug=$debug) START")
         println("     $ds salinity length: ", length(salinity))
         println("     $ds temperature length: ", length(temperature))
         println("     $ds pressure length: ", length(pressure))
