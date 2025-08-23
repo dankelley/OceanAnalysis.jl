c7fdbd926f01b1d447591c6274c9cfd0506516cc
dankelley <kelley.dan@gmail.com>
2025-08-23 07:24:19 -0300

    example code

---
+++ README-figure.jl
@@ -0,0 +1,10 @@
+# Read a built-in CTD file, and plot a profile of Absolute Salinity
+using OceanAnalysis, Plots, Measures
+pkgdir = dirname(dirname(pathof(OceanAnalysis)))
+filename = joinpath(pkgdir, "data", "ctd.cnv")
+ctd = readCtdCNV(filename, !true)
+p1 = plotProfile(ctd, "SA")
+p2 = plotProfile(ctd, "CT")
+p3 = plotTS(ctd)
+plot(p1, p2, p3, layout=(1, 3), size=(800, 400), margin=0.25cm)
+savefig("example.png")
