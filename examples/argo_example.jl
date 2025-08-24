e24a1143c6adc0247e2bd74d8d616146d27312c9
dankelley <kelley.dan@gmail.com>
2025-08-24 07:45:17 -0300

    .

--- examples/argo_example.jl
+++ examples/argo_example.jl
@@ -1,11 +1,11 @@
 # Read a built-in Argo file, and plot some hydrographic diagrams
 # %%
 using OceanAnalysis, Plots, Measures
-pkgdir = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data",
+filename = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data",
     "D4902911_095.nc")
-print(filename)
 # %%
 ctd = readArgo(filename, debug=1)
+# %%
 p1 = plotProfile(ctd, "SA", debug=1)
 p2 = plotProfile(ctd, "CT", debug=1)
 p3 = plotTS(ctd, debug=1)
