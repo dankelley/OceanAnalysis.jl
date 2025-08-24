a9e1427dd7f93b378b29a0534792e2da1ea8cd20
dankelley <kelley.dan@gmail.com>
2025-08-24 09:19:27 -0300

    .

--- examples/cnv_example.jl
+++ examples/cnv_example.jl
@@ -1,10 +1,10 @@
-# Read a built-in CTD file, and plot some standard diagrams
+# Read a built-in CTD file, and plot some hydrographic diagrams
 using OceanAnalysis, Plots, Measures
 filename = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data",
     "ctd.cnv")
-ctd = read_ctd_cnv(filename, debug=1)
-p1 = plot_profile(ctd, "SA", debug=1)
-p2 = plot_profile(ctd, "CT", debug=1)
-p3 = plot_TS(ctd, debug=1)
+ctd = read_ctd_cnv(filename)
+p1 = plot_profile(ctd, "SA")
+p2 = plot_profile(ctd, "CT")
+p3 = plot_TS(ctd)
 plot(p1, p2, p3, layout=(1, 3), size=(800, 400), margin=0.25cm)
 savefig("cnv_example.png")
