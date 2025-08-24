4764bd5d6cff0ba971bb9956dfc22f2fd87dddce
dankelley <kelley.dan@gmail.com>
2025-08-24 08:20:54 -0300

    .

--- examples/cnv_example.jl
+++ examples/cnv_example.jl
@@ -1,10 +1,10 @@
 # Read a built-in CTD file, and plot some standard diagrams
 using OceanAnalysis, Plots, Measures
-pkgdir = dirname(dirname(pathof(OceanAnalysis)))
-filename = joinpath(pkgdir, "data", "ctd.cnv")
-ctd = readCtdCNV(filename, debug=1)
-p1 = plotProfile(ctd, "SA", debug=1)
-p2 = plotProfile(ctd, "CT", debug=1)
-p3 = plotTS(ctd, debug=1)
+filename = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data",
+    "ctd.cnv")
+ctd = read_ctd_cnv(filename, debug=1)
+p1 = plot_profile(ctd, "SA", debug=1)
+p2 = plot_profile(ctd, "CT", debug=1)
+p3 = plot_TS(ctd, debug=1)
 plot(p1, p2, p3, layout=(1, 3), size=(800, 400), margin=0.25cm)
 savefig("cnv_example.png")
