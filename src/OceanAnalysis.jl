8ab3541bc9ddd7aee79cab48ba8762f96d7f7ebc
dankelley <kelley.dan@gmail.com>
2025-08-26 15:13:18 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -308,9 +308,8 @@ end
 """
     plot_TS(ctd::Ctd; sigma0_levels=[], spiciness0_levels=0,
         draw_freezing=true, abbreviate=false,
-        framestyle=:box,
-        seriestype=:scatter, seriescolor=:white, seriesalpha=0.5,
-        color=:black, legend=false, gridstyle=:dash, tickfontsize=8, labelfontsize=8,
+        framestyle=:box, color=:black, seriestype=:scatter, ms=3,
+        legend=false, gridstyle=:dash, tickfontsize=8, labelfontsize=8,
         debug::Int64=0, kwargs...)
 
 Plot an oceanographic TS diagram, with the Gibbs Seawater equation of state.
@@ -349,8 +348,8 @@ See also [`plot_profile`](@ref).
 """
 function plot_TS(ctd::Ctd; sigma0_levels=[], spiciness0_levels=0,
     draw_freezing=true, abbreviate=false,
-    framestyle=:box, seriestype=:scatter, seriescolor=:white, seriesalpha=0.5,
-    color=:black, legend=false, gridstyle=:dash, tickfontsize=8, labelfontsize=8,
+    framestyle=:box, color=:black, seriestype=:scatter, ms=3,
+    legend=false, gridstyle=:dash, tickfontsize=8, labelfontsize=8,
     debug::Int64=0, kwargs...)
     oad(debug, "plot_TS(<ctd>) START")
     local S = ctd.data.salinity
@@ -366,7 +365,7 @@ function plot_TS(ctd::Ctd; sigma0_levels=[], spiciness0_levels=0,
         xlabel=abbreviate ? "SA [g/kg]" : "Absolute Salinity [g/kg]",
         ylabel=abbreviate ? "C [°C]" : "Conservative Temperature [°C]",
         yrot=90, framestyle=framestyle,
-        seriestype=seriestype, seriescolor=seriescolor, seriesalpha=seriesalpha,
+        seriestype=seriestype, ms=ms,
         gridstyle=gridstyle, color=color, tickfontsize=tickfontsize,
         labelfontsize=labelfontsize; kwargs...)
     # ... then add density contours ...
