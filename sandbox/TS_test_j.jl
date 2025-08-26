fa4b3f220d8ec657b83c5720cf55ddcab402e3bc
dankelley <kelley.dan@gmail.com>
2025-08-26 13:58:11 -0300

    TS tests

--- sandbox/TS_test_j.jl
+++ sandbox/TS_test_j.jl
@@ -5,5 +5,6 @@ filename = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data",
 ctd = read_ctd_cnv(filename)
 
 #plot_TS(ctd, sigma0_levels=10)
-plot_TS(ctd, debug=1)
+#plot_TS(ctd, sigma0_levels=0, debug=1)
+plot_TS(ctd, spiciness0_levels=[], debug=1)
 savefig("TS_test_j.png")
