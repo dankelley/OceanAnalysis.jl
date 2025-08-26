b6c7644ac456089d13735d0ca9ab38fbd1842f0f
dankelley <kelley.dan@gmail.com>
2025-08-26 16:01:17 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -558,7 +558,7 @@ function read_ctd_cnv(stream::IOStream; debug::Int64=0)
     oad(debug, "    adding columns with standard names (e.g. 'pressure' for 'pr')")
     if "pr" in names(data)
         data.pressure = data.pr
-    elseif "prCM" in names(data)
+    elseif "prDM" in names(data)
         data.pressure = data.prDM
     else
         error("No 'pr' or 'prDM' column in CNV file; available names are ", names(data))
