98f901f4c0412394a852ffe30cdece9f19be0db7
dankelley <kelley.dan@gmail.com>
2025-08-26 09:51:25 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -366,10 +366,8 @@ function plot_TS(ctd::Ctd; draw_freezing=true, draw_spiciness=false,
     CTc = range(ylim[1], ylim[2], length=300)
     oad(debug, "    drawing sigma0 contours")
     sigma0c = gsw_sigma0.(SAc', CTc)
-    println("sigma0c follows")
-    println(sigma0c)
-    println("pretty(sigma0c) follows")
-    println(pretty(sigma0c))
+    println("size(sigma0c) $(size(sigma0c))")
+    println("pretty(sigma0c) $(pretty(sigma0c))")
     if false # FIXME
         contour!(SAc, CTc, sigma0c, color=:gray50, linewidth=1.0,
             levels=isnothing(sigma0_levels) ? pretty(sigma0c) : sigma0_levels,
