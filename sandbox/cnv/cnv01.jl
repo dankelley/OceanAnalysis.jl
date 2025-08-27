b33e00da73a6813277e7a926fc6cd5618cebc221
dankelley <kelley.dan@gmail.com>
2025-08-27 10:07:42 -0300

    read_ctd_cnv() works on all but 1 of my test files
    The broken one has a problem in the TS plot. I'll look into that.  It
    seems that the densities are all NaN (or maybe extremum() needs to be
    told to ignore NaN values).

--- sandbox/cnv/cnv01.jl
+++ sandbox/cnv/cnv01.jl
@@ -1,14 +1,21 @@
-using OceanAnalysis, Glob
+using OceanAnalysis, Glob, Plots
 files = glob("*.cnv", "/Users/kelley/Dropbox/oce-working-notes/cnv")
 files = sort(files)
+files_short_name = replace.(files, r".*/" => "", ".cnv" => "")
 bad = 0
-for file in files
+for i in 1:length(files)
     #println("$file")
+    file = files[i]
+    file_short_name = files_short_name[i]
     try
         d = read_ctd_cnv(file)
+        plot_TS(d, title=file, titlefont=font(9))
+        png = "conv01_$(file_short_name).png"
+        println(png)
+        savefig(png)
     catch e
         global bad = bad + 1
-        println("    $e")
+        println("$file\n    $e")
     end
 end
 println("Got errors in $bad of the $(length(files)) files")
--- sandbox/sw/conductivity.jl
+++
@@ -1,4 +0,0 @@
-using OceanAnalysis, Test
-# Test value from TEOS10 website (also used in R package "gsw")
-println(salinity_from_conductivity(34.5487, 28.7856, 10.0))
-
