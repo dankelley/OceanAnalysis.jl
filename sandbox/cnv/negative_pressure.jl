facc4c526d3b01dcd47a021ccb7b7adfa53e6ded
dankelley <kelley.dan@gmail.com>
2025-08-27 13:41:17 -0300

    .

--- sandbox/cnv/negative_pressure.jl
+++ sandbox/cnv/negative_pressure.jl
@@ -2,15 +2,17 @@
 using OceanAnalysis, Plots
 f = "/Users/kelley/Dropbox/oce-working-notes/cnv/S262-023-CTD.cnv"
 d = read_ctd_cnv(f);
-
-p = d.data.pressure
-bad = sum(p .< 0)
-histogram(p[p.<0],
-    title="Have $bad negative pressures, or $(round(100*bad/length(p))) percent of values",
-    titlefont=font(10))
-s0 = d.data.sigma0
-
-extrema(filter(!isnan, s0))
-
-plot_TS(d, debug=1)
-
+n = length(d.data.pressure)
+println(names(d.data))
+dd = d.data[:, ["pressure", "c0mS/cm", "salinity", "temperature", "SA", "CT", "sigma0", "spiciness0"]]
+println(first(dd, 4))
+println(last(dd, 4))
+if false
+    p = d.data.pressure
+    bad = sum(p .< 0)
+    histogram(p[p.<0],
+        title="Have $bad negative pressures, or $(round(100*bad/length(p))) percent of values",
+        titlefont=font(10))
+    s0 = d.data.sigma0
+    plot_TS(d)
+end
