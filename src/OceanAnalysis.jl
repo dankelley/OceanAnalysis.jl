2d0c047d9164f4ad0b010452d46b80d4153745cf
dankelley <kelley.dan@gmail.com>
2025-08-27 19:31:08 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -520,11 +520,11 @@ function read_argo(filename, column=1; debug::Int64=0)
     #temperature = convert(Vector{Float64}, d["TEMP"][:, column])
     temperature = get_nc_value(d["TEMP"][:, column])
     oad(debug, "    temperature length: $(length(temperature))")
-    longitude = convert(Float64, d["LONGITUDE"][1])
-    #longitude = get_nc_value(d["LONGITUDE"][1])
+    #longitude = convert(Float64, d["LONGITUDE"][1])
+    longitude = get_nc_value(d["LONGITUDE"][1])
     oad(debug, "    longitude: $longitude")
-    latitude = convert(Float64, d["LATITUDE"][1])
-    #latitude = get_nc_value(d["LATITUDE"][1])
+    #latitude = convert(Float64, d["LATITUDE"][1])
+    latitude = get_nc_value(d["LATITUDE"][1])
     oad(debug, "    latitude: $latitude")
     rval = as_ctd(salinity, temperature, pressure, longitude, latitude,
         debug=debug > 0 ? debug + 1 : 0)
@@ -534,7 +534,10 @@ end # read_argo()
 
 function get_nc_value(item)
     item[ismissing.(item)] .= NaN
-    return convert(Vector{Float64}, item)
+    if length(item) > 1
+        return convert(Vector{Float64}, item)
+    else
+        return convert(Vector, item)
 end
 
 
