12904a8f54278b0ed678b2fe959dce8ae23d5175
dankelley <kelley.dan@gmail.com>
2025-08-28 15:08:36 -0300

    Improve test plot (no need for profiles)

--- sandbox/argo/argo_01.jl
+++ sandbox/argo/argo_01.jl
@@ -4,22 +4,8 @@ using OceanAnalysis, Plots
 f = "/Users/kelley/data/argo/D6902967_095.nc"
 d = read_argo(f, debug=1)
 println("$f: $(length(d.data.pressure)) levels on $(d.metadata["time"]) at $(round(d.metadata["latitude"], digits=4))N, $(round(d.metadata["longitude"], digits=4))E")
-#println(d.data.pressure)
-println(d.data)
-
-
-
-
-
-
-
-
-
-# %%
-
-p1 = plot_profile(d, "CT")
-p2 = plot_profile(d, "SA")
-p3 = plot_TS(d)
-plot(p1, p2, p3, layout=(1, 3), size=(800, 800))
+println(first(d.data, 3))
+println(last(d.data, 3))
+plot_TS(d)
 savefig("argo_01.png")
 
