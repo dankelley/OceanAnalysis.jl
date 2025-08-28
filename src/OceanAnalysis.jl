5ee7a617a59891c2095af251d068d143cc5b313a
dankelley <kelley.dan@gmail.com>
2025-08-28 14:01:35 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -49,10 +49,11 @@ function oad(debug::Int64=0, args...)
     end
 end
 
-export fix_gsw_bad_code!
-function fix_gsw_bad_code!(x)
-    x[x.>1e15] .= NaN
-    x
+export fix_gsw_bad_code
+function fix_gsw_bad_code(x)
+    rval = x
+    rval[rval.>1e15] .= NaN
+    rval
 end
 
 """
