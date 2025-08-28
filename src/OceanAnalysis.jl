04abaa81e9f0813de50c419b355055d05243f173
dankelley <kelley.dan@gmail.com>
2025-08-28 13:52:19 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -198,7 +198,7 @@ function as_ctd(salinity::Vector{Float64}, temperature::Vector{Float64}, pressur
     oad(debug, "    given latitude (length: $(length(latitude)))")
     local SA = gsw_sa_from_sp.(salinity, pressure, longitude, latitude)
     oad(debug, "    FIRST  created SA (length: $(length(SA)), ends: $(last(SA, 6))")
-    SA[SA.>1e15] = NaN
+    SA[SA.>1e15] .= NaN
     oad(debug, "    LATER created SA (length: $(length(SA)), ends: $(last(SA, 6))")
     local CT = gsw_ct_from_t.(SA, temperature, pressure)
     oad(debug, "    created CT (length: $(length(CT)), max: $(maximum(filter(!isnan, CT))))")
