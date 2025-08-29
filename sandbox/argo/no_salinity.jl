80ecf30e46b5edc75191168d6e4623dc3d9f55d4
dankelley <kelley.dan@gmail.com>
2025-08-29 12:00:05 -0300

    .

--- sandbox/argo/no_salinity.jl
+++ sandbox/argo/no_salinity.jl
@@ -4,22 +4,23 @@
 # salinity.
 using OceanAnalysis
 using NCDatasets
-#files = ("BR4902576_017.nc", "BR4902577_016.nc")
-file = "BR4902576_017.nc"
-if isfile(file)
-    println("File: ", file, "\n")
-    try
-        d = read_argo(file, debug=1)
-    catch e
-        println("problem with file ($e)")
+files = ("BR4902576_017.nc", "BR4902577_016.nc")
+for file in files
+    if isfile(file)
+        println("File: ", file, "\n")
+        try
+            d = read_argo(file, debug=1)
+            println("data starts: $(first(d.data, 3))")
+        catch e
+            println("problem with file ($e)")
+        end
+        # data = NCDataset(file, "r") do ds
+        #     if haskey(ds, "PSAL")
+        #         println(" -- have salinity")
+        #     end
+        #     if haskey(ds, "TEMP")
+        #         println(" -- have temperature")
+        #     end
+        # end
     end
-    println("data starts: $(first(d.data, 3))")
-    # data = NCDataset(file, "r") do ds
-    #     if haskey(ds, "PSAL")
-    #         println(" -- have salinity")
-    #     end
-    #     if haskey(ds, "TEMP")
-    #         println(" -- have temperature")
-    #     end
-    # end
 end
