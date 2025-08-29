a76bebe7526ed4b6e0eb51816c0bb34e78ebcb88
dankelley <kelley.dan@gmail.com>
2025-08-29 10:23:34 -0300

    .

--- data/tmp.jl
+++ data/tmp.jl
@@ -1,2 +1,4 @@
-using OceanAnalysis
-a = read_argo("D4902911_095.nc")
+using NCDatasets
+d = NCDataset("D4902911_095.nc", "r")
+platform = replace(join(d["PLATFORM_NUMBER"][:, 1]), "missing" => "")
+println("'$platform'")
