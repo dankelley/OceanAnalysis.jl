1b366d8a13403f12c8c675769b5db9e8f111202e
dankelley <kelley.dan@gmail.com>
2025-08-30 09:39:55 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -222,7 +222,7 @@ end
 """
     as_ctd(salinity, temperature, pressure, longitude=-30.0, latitude=30.0; time, debug::Int64=0)
 
-    Construct a [`Ctd`](@ref) object, given S, T, p, and a location.
+Construct a [`Ctd`](@ref) object, given S, T, p, and a location.
 
 This returns a `Ctd` object with a `data` element that is a data frame holding
 the provided water properties, along with computed Absolute Salinity (`SA`)
@@ -673,15 +673,18 @@ end
 
 Read a CTD file named `filename` that is in SeaBird CNV format.
 
-Returns items `metadata` (a Dict) and `data` (a DataFrame).
+Returns a [`Ctd`](@ref) object that holds `metadata` (a Dict) and `data` (a
+DataFrame).
 
-The `metadata` item of the return value holds `header` (a vector of strings,
-one per line from the start down to a line containing `#END`), plus some
-particular items scanned from that header. The `data` item holds the columnar
-data read from the file, along with renamed values in standard nomenclature. At
-present, the only renamed items are salinity, temperature, and pressure.
-Note that if the data file indicates temperature is on the T68 scale,
-then [`read_ctd_cnv`] converts to T90 before saving as `temperature`. 
+* `metadata` holds `header` (a vector of strings, one per line from the start
+down to a line containing `#END`), plus some particular items scanned from that
+header.
+
+* `data` holds the columnar data read from the file, along with renamed values
+in standard nomenclature. At present, the only renamed items are salinity,
+temperature, and pressure. Note that if the data file indicates temperature is
+on the T68 scale, then [`read_ctd_cnv`] converts to T90 before saving as
+`temperature`. 
 
 # Examples
 ```julia-repl
