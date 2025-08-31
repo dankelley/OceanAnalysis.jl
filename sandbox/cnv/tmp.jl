90b6d6670901a508653b9a46e804cf5a86ef0ac5
dankelley <kelley.dan@gmail.com>
2025-08-31 10:20:00 -0300

    .

--- sandbox/cnv/tmp.jl
+++ sandbox/cnv/tmp.jl
@@ -4,4 +4,4 @@ file = "/Users/kelley/git/OceanAnalysis.jl/data/ctd.cnv"
 file = "/Users/kelley/data/arctic/beaufort/2012/d201211_0056.cnv"
 println("file", file)
 d = read_ctd_cnv(file, debug=1)
-println(d.metadata)
+println(keys(d.metadata))
