ae5f1a60f399167640a210c81318ad7865a3e214
dankelley <kelley.dan@gmail.com>
2025-08-31 15:00:18 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -762,7 +762,7 @@ function read_ctd_cnv(stream::IOStream, filename::String=""; debug::Int64=0)
             println("2. sign=", sign)
             line = replace(line, r"[NSns]" => "") |> strip
             println("3. after remove hemisphere line='", line, "'")
-            s = split(line, ": ")[2]
+            s = split(line, ": ")[2] |> strip
             println("4. s=", s)
             ss = split(s, r"[ ]+")
             println("5. ss= ", ss)
