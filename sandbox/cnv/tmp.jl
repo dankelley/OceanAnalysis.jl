e4a2900e483f89076de26ac12c7c2d3d57a72ea2
dankelley <kelley.dan@gmail.com>
2025-08-31 12:13:30 -0300

    .

--- sandbox/cnv/tmp.jl
+++ sandbox/cnv/tmp.jl
@@ -5,3 +5,5 @@ file = "/Users/kelley/data/arctic/beaufort/2004/d200416_049.cnv"
 println(file)
 d = read_ctd_cnv(file)
 println(": ", d.metadata["time"], " @ ", d.metadata["latitude"], "N, ", d.metadata["longitude"], "E\n")
+
+println(d)
