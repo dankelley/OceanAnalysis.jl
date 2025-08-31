d99e9b14fbbcd079c0d153524cc169addf686af9
dankelley <kelley.dan@gmail.com>
2025-08-31 15:56:23 -0300

    tst file

--- sandbox/cnv/tmp.jl
+++ sandbox/cnv/tmp.jl
@@ -1,19 +1,12 @@
-using OceanAnalysis, Dates, Plots
+using OceanAnalysis
 files = ["/Users/kelley/data/arctic/beaufort/2005/d200504_001.cnv";
     "/Users/kelley/data/arctic/beaufort/2005/d200504_005.cnv";
     "/Users/kelley/Dropbox/oce-working-notes/cnv/AS_CTD_20130821_c043.cnv"]
-
-files = ["/Users/kelley/data/arctic/beaufort/2005/d200504_001.cnv"] # lon and lat are okay
-files = ["/Users/kelley/data/arctic/beaufort/2005/d200504_005.cnv"] # lon and lat are okay
-files = ["/Users/kelley/Dropbox/oce-working-notes/cnv/AS_CTD_20130821_c043.cnv"] # no lon or lat
-
 for file in files
     d = read_ctd_cnv(file, debug=0)
     println(d.metadata["filename"], " @ ", d.metadata["latitude"], " N and ", d.metadata["longitude"], " E")
     println("  first(d.data, 2)")
     println(first(d.data, 2))
-    #print(keys(d.metadata))
-    #println(names(d.data))
-    #print(first(d.data, 3))
-    #plot_TS(d)
+    print(keys(d.metadata))
+    print(first(d.data, 2))
 end
