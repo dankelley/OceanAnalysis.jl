3eaf50b83a6389775e7492131082dce187686c2b
dankelley <kelley.dan@gmail.com>
2025-08-31 15:47:32 -0300

    .

--- sandbox/cnv/failure_to_read.jl
+++ sandbox/cnv/failure_to_read.jl
@@ -2,3 +2,7 @@ using OceanAnalysis
 files = ["/Users/kelley/Dropbox/oce-working-notes/cnv/JR302_001_align_ctm.cnv";
     "/Users/kelley/Dropbox/oce-working-notes/cnv/first_CTD_cast.cnv"]
 d = read_ctd_cnv(files[1], debug=1)
+println(d.metadata["filename"], " @ ", d.metadata["latitude"], " N and ", d.metadata["longitude"], " E")
+println("  first(d.data, 2)")
+println(first(d.data, 2))
+
