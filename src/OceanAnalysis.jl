fd115f0cbee5cbd3d761b02811a7c7ffccb1e081
dankelley <kelley.dan@gmail.com>
2025-09-05 16:51:26 -0300

    .

--- src/OceanAnalysis.jl
+++ src/OceanAnalysis.jl
@@ -1,5 +1,4 @@
 """
-
 The OceanAnalysis module is intended to help with the analysis of oceanographic
 data. It is in a preliminary form, providing help with only two file types:
 Argo NetCDF files and CTD files in the Seabird CNV format.  In neither case
@@ -18,68 +17,13 @@ that it can locate information whether it is in the `data` or the `metadata`
 component of the object. It is also possible to obtain derived information,
 e.g. `ctd["SA"]` calculates and then returns the Absolute Salinity, which is
 not typically stored in CTD files, but which can be computed from the stored
-information. Other derivable items are `"CT"` (Conservative Temperature),
+information. Other derivable items include `"CT"` (Conservative Temperature),
 `"sigma0"` (potential density anomaly with respect to surface pressure),
 and `spiciness0` (spiciness).
-
 """
 module OceanAnalysis
 
-using NCDatasets
-using Dates
-using DataFrames
-using GibbsSeaWater
-using Plots
-using CSV
-using Dierckx
-using Statistics
-
-# Structs
-export OA
-export Ctd
-#. export Argo
-
-# Functions
-export argo_id_cycle
-export as_ctd
-export coordinate_from_string
-export CT
-export depth_from_pressure
-export fix_gsw_bad_code
-export fix_gsw_bad_code!
-export get_element
-export N2
-export plot_profile
-export plot_TS
-export pressure_from_depth
-export pressure_from_z
-export pretty
-export read_argo
-export read_ctd_cnv
-export salinity_from_conductivity
-export SA
-export T90_from_T48
-export T90_from_T68
-export z_from_pressure
-
-"""
-    Base object in the OceanAnalysis package.
-"""
-abstract type OA end
-
-"""
-    An object to hold CTD data
-
-Ctd is an object to store data from CTD instruments. As a class that derives
-from the base object of the package, it is a struct that holds a Dict named
-`metadata` and a DataFrame named `data`.  See [`OA`](@ref) for general notes on
-the data structure and its access.
-"""
-struct Ctd <: OA
-    metadata::Dict{String,Any}
-    data::DataFrames.DataFrame
-end
-
+include("setup.jl")
 include("argo.jl")
 include("ctd.jl")
 include("plot.jl")
