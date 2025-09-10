0fcef4869bfe3a29ecf0999c07ce0c17afa4f497
dankelley <kelley.dan@gmail.com>
2025-09-10 07:41:37 -0300

    GMT test

---
+++ sandbox/map/map_01.jl
@@ -0,0 +1,7 @@
+using GMT, Plots
+R = [-68, -59, 42, 47.5]
+C = [0.5 * (R[1] + R[2]), 0.5 * (R[3] + R[4])]
+P = :aea
+p = [40 50]
+coast(region=R, proj=(name=P, center=C, parallels=p), figsize=10)
+text!(text="Nova Scotia", x=-62.9, y=45.3, font=10, angle=22, show=1, savefig="map_01.png")
