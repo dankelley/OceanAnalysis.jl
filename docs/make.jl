770c565ea393e3ae84373c0ff86c269e11e4af98
dankelley <kelley.dan@gmail.com>
2025-10-02 12:59:19 -0300

    Minor changes (do docs build on GH?)

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,15 @@
 # of which patiently and generously helped me to learn how to build Julia
 # documentation.
 
+# Next from https://raw.githubusercontent.com/JuliaManifolds/Manopt.jl/72ecc6d72bc191de273506af75bef215fc836191/docs/make.jl
+if Base.active_project() != joinpath(@__DIR__, "Project.toml")
+    using Pkg
+    Pkg.activate(@__DIR__)
+    Pkg.develop(PackageSpec(; path=(@__DIR__) * "/../"))
+    Pkg.resolve()
+    Pkg.instantiate()
+end
+
 using Documenter, OceanAnalysis
 
 DocMeta.setdocmeta!(OceanAnalysis, :DocTestSetup, :(using OceanAnalysis); recursive=true)
@@ -15,10 +24,10 @@ makedocs(;
     sitename="OceanAnalysis.jl",
     checkdocs=:none,
     format=Documenter.HTML(;
-      assets = String["assets/custom.css"],
-      canonical = "https::dankelley.github.io/OceanAnalysis.jl/dev/",
-      repolink = "https://github.com/dankelley/OceanAnalysis.jl",
-      prettyurls=get(ENV, "CI", "false") == "true",
+        assets=String["assets/custom.css"],
+        canonical="https::dankelley.github.io/OceanAnalysis.jl/dev/",
+        repolink="https://github.com/dankelley/OceanAnalysis.jl",
+        prettyurls=get(ENV, "CI", "false") == "true",
     ),
     pages=[
         "Home" => "index.md",
