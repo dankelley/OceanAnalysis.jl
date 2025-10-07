3e137468c0793819afcc44e0c4da7a29ba0a2d32
Documenter.jl <documenter@juliadocs.github.io>
2025-10-07 21:31:54 +0000

    build based on 3b7dc09

--- dev/argo_profile.jl
+++ dev/argo_profile.jl
@@ -1,13 +1,12 @@
-# %% Read a built-in Argo file, and plot some hydrographic diagrams
+# Read and plot a built-in Argo file
 using OceanAnalysis, Plots, Measures, Dates
 filename = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data",
     "D4902911_095.nc")
 a = read_argo(filename)
-# %% Plot an overview of hydrographic properties
-p1 = plot_profile(a, which="CT")
-p2 = plot_profile(a, which="SA")
-p3 = plot_profile(a, which="sigma0")
-p4 = plot_TS(a)
+p1 = plot_profile(a, which="CT");
+p2 = plot_profile(a, which="SA");
+p3 = plot_profile(a, which="sigma0");
+p4 = plot_TS(a);
 title = "CTD observations at " *
         "$(round(a.metadata["latitude"],digits=3))N and " *
         "$(round(a.metadata["longitude"],digits=3))E" *
