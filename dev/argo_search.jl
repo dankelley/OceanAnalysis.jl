0dc17bb0c5f25acb2fb145317617abb3dd84169a
Documenter.jl <documenter@juliadocs.github.io>
2025-10-15 21:36:58 +0000

    build based on d1d56b4

---
+++ dev/argo_search.jl
@@ -0,0 +1,32 @@
+# Show Argo profiles within 200 km of Sable Island in last year
+using OceanAnalysis, CSV, Dates, DataFrames, Plots
+# Get the index
+index_file = get_argo_index("~/data/argo")
+index_all = read_argo_index(index_file) # 3.2e6 profiles
+# Set time subset
+today = now(UTC)
+start = today - Dates.Year(1)
+recent = start .< index_all.time .< today
+# Set distance subset
+SI_lon = -59.915
+SI_lat = 43.934
+radius = 200.0 # km
+distance = map(i -> geod_distance(SI_lon, SI_lat,
+        index_all.longitude[i], index_all.latitude[i]),
+    1:nrow(index_all))
+near = distance .< radius
+# Filter by both time and distance
+index = index_all[recent.&near, :]
+# Extend region of map to show geographic context
+aspect_ratio = 1.0 / cos(SI_lat * pi / 180.0)
+scale = radius / 111.
+scatter(index.longitude, index.latitude,
+    xlims=SI_lon .+ scale .* (-1.2, 1.2) .* aspect_ratio,
+    ylims=SI_lat .+ scale .* (-1.2, 1.2),
+    aspect_ratio=aspect_ratio,
+    tickdirection=:out, framestyle=:box, dpi=200, legend=false)
+plot_coastline!(coastline())
+# Print the float IDs in this subset
+float_IDs = replace.(index.file, r".*/(.*)_.*" => s"\1") |> unique
+show(float_IDs)
+savefig("argo_search.png")
