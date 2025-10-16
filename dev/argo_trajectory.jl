9620b2ed6baaf47986f1bb0242cd33fbb6da56bf
Documenter.jl <documenter@juliadocs.github.io>
2025-10-16 12:47:03 +0000

    build based on 11a69c4

--- dev/argo_trajectory.jl
+++ dev/argo_trajectory.jl
@@ -21,4 +21,5 @@ topo = read_topography(topo_file)
 contour!(topo.metadata["longitude"], topo.metadata["latitude"],
     topo.data, xlim=xlims(), ylim=ylims(),
     color=:gray, linewidth=2, colorbar_entry=false, levels=[-1000.0])
+scale_bar(500, :right, :top)
 savefig("argo_trajectory.png")
