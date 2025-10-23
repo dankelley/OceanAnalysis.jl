eb9dc13da4ceaeff08df11545ded52634f3b84d7
dankelley <kelley.dan@gmail.com>
2025-10-23 17:06:55 -0300

    map

--- sandbox/map/halifax_harbour.jl
+++ sandbox/map/halifax_harbour.jl
@@ -1,7 +1,7 @@
 using GMT, Plots
-R = [-63 - 42 / 60, -63 - 25 / 60, 44 + 33 / 60, 44 + 45 / 60]
+R = [-63 - 42 / 60, -63 - 25 / 60, 44 + 33 / 60, 44 + 44 / 60]
 C = [0.5 * (R[1] + R[2]), 0.5 * (R[3] + R[4])]
 P = :aea
 p = [44 48]
-coast(region=R, proj=(name=P, center=C, parallels=p), figsize=10, file="nova_scotia.png")
-text!(text="B.B.", x=-63 - 38 / 60, y=44 + 41.5 / 60, angle=-47, font=8, show=1)
+P = coast(region=R, proj=(name=P, center=C, parallels=p), figsize=10)
+text!(text="B.B.", x=-63 - 38 / 60, y=44 + 41.5 / 60, angle=-47, font=8, show=1, savefig="nova_scotia.png")
