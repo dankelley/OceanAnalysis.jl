7efd2e13e3eb1c2f01ed6c3ec463d7d14e0fb29e
dankelley <kelley.dan@gmail.com>
2025-11-13 14:45:34 -0400

    nova_scotia.jl works now

--- sandbox/map/nova_scotia.jl
+++ sandbox/map/nova_scotia.jl
@@ -3,6 +3,8 @@ R = [-68, -59, 42, 47.5]
 C = [0.5 * (R[1] + R[2]), 0.5 * (R[3] + R[4])]
 P = :aea
 p = [40 50]
-coast(region=R, proj=(name=P, center=C, parallels=p), figsize=10)
-text!(text="Nova Scotia", x=-62.9, y=45.3, font=10, angle=22, show=1)
-savefig="nova_scotia.png")
+coast(region=R, proj=(name=P, center=C, parallels=p), figsize=10, land=:gray)#, savefig="nova_scotia.png")
+x = [-64.5; -64.6]
+y = [45.5; 45.6]
+GMT.scatter!(x, y, show=true)
+#savefig("bark.png")
