5e0871d9eba972b1453816696fad46aba60a9c22
dankelley <kelley.dan@gmail.com>
2025-11-16 11:18:36 -0400

    plot_section() improvements

--- src/plot.jl
+++ src/plot.jl
@@ -185,7 +185,7 @@ end
 """
     plot_section(x, y, z, levels=:auto;
         title="", xlab="Distance from Shore [km]", ylab="Pressure [db]",
-        font_size=8, show_stations=true, kwargs...)
+        font_size=8, show_stations=true, dpi=200, kwargs...)
 
 Draw an oceanographic section plot, with contours for `z` as a function
 of `x` and `y`. This is a preliminary version of the function, subject
@@ -197,7 +197,7 @@ on that. The other options ought to be reasonably self-explanatory.
 """
 function plot_section(x, y, z, levels=:auto;
     title="", xlab="Distance from Shore [km]", ylab="Pressure [db]",
-    font_size=8, show_stations=true, kwargs...)
+    font_size=8, show_stations=true, dpi=200, kwargs...)
     if levels == :auto
         levels = pretty(z, 10)
     end
@@ -205,12 +205,12 @@ function plot_section(x, y, z, levels=:auto;
         xlab=xlab, ylab=ylab, title=title, titlelocation=:left,
         framestyle=:box, levels=levels, cbar=false, clabels=true,
         tickdirection=:out, titlefontsize=font_size, labelfontsize=font_size, tickfontsize=font_size,
-        kwargs...)
+        dpi=dpi, kwargs...)
     if show_stations
         xlim, ylim = xlims(), ylims()
         for xx in x
             plot!(repeat([xx], 2), collect(ylim), xlim=xlim, ylim=ylim,
-                seriestype=:path, color=:lightgray, linewidth=0.5, grid=false, label=false)
+                seriestype=:path, color=:lightgray, linewidth=0.75, grid=false, label=false)
         end
     end
     rval
