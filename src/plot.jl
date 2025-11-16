d9080cac8b2c81b83c0abd01c1b9589d19095706
dankelley <kelley.dan@gmail.com>
2025-11-16 11:12:33 -0400

    plot_section() improvements

--- src/plot.jl
+++ src/plot.jl
@@ -185,23 +185,27 @@ end
 """
     plot_section(x, y, z, levels=:auto;
         title="", xlab="Distance from Shore [km]", ylab="Pressure [db]",
-        figure_size=(600, 400), font_size=8, show_stations=true)
+        font_size=8, show_stations=true, kwargs...)
 
-Draw an oceanographic section plot, with contours for z=z(x,y). This is a
-preliminary version of the function, subject to changes.  More documentation
-will be added later, after a period of real-world testing and modification.
+Draw an oceanographic section plot, with contours for `z` as a function
+of `x` and `y`. This is a preliminary version of the function, subject
+to changes that are suggested by every-day work.
+
+Note that `x` and `y` must each be ordered, because `contour()` insists
+on that. The other options ought to be reasonably self-explanatory.
+
 """
 function plot_section(x, y, z, levels=:auto;
     title="", xlab="Distance from Shore [km]", ylab="Pressure [db]",
-    figure_size=(600, 400), font_size=8, show_stations=true)
+    font_size=8, show_stations=true, kwargs...)
     if levels == :auto
         levels = pretty(z, 10)
     end
     rval = contour(x, y, z, yflip=true, color=:black,
         xlab=xlab, ylab=ylab, title=title, titlelocation=:left,
         framestyle=:box, levels=levels, cbar=false, clabels=true,
-        size=figure_size, tickdirection=:out,
-        titlefontsize=font_size, labelfontsize=font_size, tickfontsize=font_size, dpi=dpi)
+        tickdirection=:out, titlefontsize=font_size, labelfontsize=font_size, tickfontsize=font_size,
+        kwargs...)
     if show_stations
         xlim, ylim = xlims(), ylims()
         for xx in x
