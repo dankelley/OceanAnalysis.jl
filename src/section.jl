c924ca698095e34ef5afacd84e7abd40fca6a6db
dankelley <kelley.dan@gmail.com>
2025-12-24 09:32:03 -0400

    Restrict as_section args BUT
    I am still not sure if I want Vector or Tuple, or maybe accept either.

--- src/section.jl
+++ src/section.jl
@@ -1,5 +1,5 @@
 """
-    as_section(ctds; name="", source="", debug=0)
+    as_section(ctds::Vector{Ctd}; name::String="", source::String="", debug::Int64=0)
 
 Construct a Section object, which olds one or more Ctd objects.
 
@@ -22,41 +22,22 @@ the `name` of the section and the `source` of the data.
 # Examples
 ```julia
 julia> using OceanAnalysis
-
-julia> # Create fake data (with S and T varying between stations)
+julia> # Create fake data
        f = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data", "ctd.cnv");
-
 julia> a = read_ctd_cnv(f, add_teos=false);
-
 julia> b = read_ctd_cnv(f, add_teos=false);
-
 julia> b.data.salinity = 1.0 .+ b.data.salinity;
-
-julia> b.data.temperature = 1.0 .+ b.data.temperature;
-
-julia> s = as_section((a, b), name="Test");
-
-julia> println("Section \""* s.metadata["name"] * "\" has entries with data starting:")
-Section "Test" has entries with data starting:
-
+julia> s = as_section([a; b], name="Test");
+julia> println("Section '"* s.metadata["name"] * "' has entries with salinities starting:")
+Section 'Test' has entries with salinities starting:
 julia> for i in 1:length(s.data)
-           println(first(s.data[i].data, 2))
+           println(first(s.data[i].data.salinity,3))
        end
-2×10 DataFrame
- Row │ salinity  temperature  pressure  scan     timeS    pr       depS     t090     sal00    flag
-     │ Float64   Float64      Float64   Float64  Float64  Float64  Float64  Float64  Float64  Float64
-─────┼────────────────────────────────────────────────────────────────────────────────────────────────
-   1 │  29.921       14.2245     1.48     130.0    129.0    1.48     1.468  14.2245  29.921       0.0
-   2 │  29.9205      14.2299     1.671    131.0    130.0    1.671    1.657  14.2299  29.9205      0.0
-2×10 DataFrame
- Row │ salinity  temperature  pressure  scan     timeS    pr       depS     t090     sal00    flag
-     │ Float64   Float64      Float64   Float64  Float64  Float64  Float64  Float64  Float64  Float64
-─────┼────────────────────────────────────────────────────────────────────────────────────────────────
-   1 │  30.921       15.2245     1.48     130.0    129.0    1.48     1.468  14.2245  29.921       0.0
-   2 │  30.9205      15.2299     1.671    131.0    130.0    1.671    1.657  14.2299  29.9205      0.0
+[29.921, 29.9205, 29.9206]
+[30.921, 30.9205, 30.9206]
 ```
 """
-function as_section(ctds; name="", source="", debug=0)
+function as_section(ctds::Vector{Ctd}; name::String="", source::String="", debug::Int64=0)
     oad(debug, "as_section() START")
     nctds = length(ctds)
     nctds > 0 || error("  as_section() provided with zero-length first argument")
