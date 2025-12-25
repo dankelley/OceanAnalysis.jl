99bd243ad82a5a42c94b8c09db4c27755ccbc02d
Documenter.jl <documenter@juliadocs.github.io>
2025-12-25 12:53:48 +0000

    build based on 0a89ec4

--- dev/section_map.jl
+++ dev/section_map.jl
@@ -2,8 +2,8 @@ using OceanAnalysis, Plots
 url = "https://cchdo.ucsd.edu/data/11852/ar07_74JC20140606_ct1.zip"
 dir = get_section(url)
 section = read_section(dir);
-longitude = map(ctd -> get_element(ctd, "longitude"), section.data);
-latitude = map(ctd -> get_element(ctd, "latitude"), section.data);
+longitude = get_element(section, "longitude");
+latitude = get_element(section, "latitude");
 plot(longitude, latitude,
     aspect_ratio=1.0 / cos(0.5 * sum(extrema(latitude)) * pi / 180),
     seriestype=:scatter, framestyle=:box, legend=false, ms=2)
