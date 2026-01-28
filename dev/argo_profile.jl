827a2faa83dd11b0f1f13aea4f37319f8bc3c64c
Documenter.jl <documenter@juliadocs.github.io>
2026-01-28 13:43:28 +0000

    build based on 1c17a31

--- dev/argo_profile.jl
+++ dev/argo_profile.jl
@@ -1,16 +1,17 @@
 # Read and plot a built-in Argo file
-using OceanAnalysis, Plots, Measures, Dates
+using OceanAnalysis, Dates, Measures, Plots, Printf
 filename = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data",
     "D4902911_095.nc")
-a = read_argo(filename)
-p1 = plot_profile(a, which="CT");
-p2 = plot_profile(a, which="SA");
-p3 = plot_profile(a, which="sigma0");
-p4 = plot_TS(a);
-title = "CTD observations at " *
-        "$(round(a.metadata["latitude"],digits=3))N and " *
-        "$(round(a.metadata["longitude"],digits=3))E" *
-        " on $(Dates.format(a.metadata["time"], "yyyy-mm-dd"))"
+argo = read_argo(filename)
+ctd = as_ctd(argo)
+p1 = plot_profile(ctd, which="CT");
+p2 = plot_profile(ctd, which="SA");
+p3 = plot_profile(ctd, which="sigma0");
+p4 = plot_TS(ctd);
+title = @sprintf("CTD observations at %.3fN and %.3fE, on %s",
+    ctd.metadata["latitude"], ctd.metadata["longitude"],
+    Dates.format(ctd.metadata["time"], "yyyy-mm-dd"))
 plot(p1, p2, p3, p4, layout=(2, 2), size=(800, 600), margin=0.25cm,
-    dpi=200, plot_title=title, plot_titlefontsize=11)
+    dpi=200, plot_title=title, plot_titlefontsize=9)
 savefig("argo_profile.png")
+
