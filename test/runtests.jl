aa9610995ae3a6fb6b3b35d52dcce90f7897ae54
dankelley <kelley.dan@gmail.com>
2026-01-28 09:44:07 -0400

    alter test for Argo example (it reads *way* more data now)

--- test/runtests.jl
+++ test/runtests.jl
@@ -70,7 +70,9 @@ end
     @test argo.metadata["latitude"] ≈ 40.45216 atol = 1e-13
     @test argo.metadata["time"] == Dates.DateTime("2019-10-14T23:43:44.003")
     @test 1014 == length(argo.data.pressure)
-    @test collect(first(argo.data)) ≈ [34.913; 19.513; 0.48] atol = 0.0001
+    @test first(argo.data.salinity) ≈ 34.913 atol = 0.0001
+    @test first(argo.data.temperature) ≈ 19.513 atol = 0.0001
+    @test first(argo.data.pressure) ≈ 0.48 atol = 0.0001
 end
 
 
