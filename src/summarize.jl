9d39042f6b9164770adc514e90819b4a9391696f
dankelley <kelley.dan@gmail.com>
2026-02-20 09:50:41 -0400

    summarize() improvements

--- src/summarize.jl
+++ src/summarize.jl
@@ -10,6 +10,29 @@ function four_num(x, name)
     (name, Min, Mean, Max, number_missing, number_nan)
 end
 
+function summarize_data(x)
+    if x.data isa DataFrame
+        println("\nData: a DataFrame with contents as follows")
+        df = DataFrame(name=String[], Min=Float64[], Max=Float64[], Mean=Float64[],
+            num_missing=Int64[], num_nan=Int64[])
+        for name in names(x.data)
+            push!(df, four_num(x[name], name))
+        end
+        indent = "  "
+        println(indent, replace(string(df), "\n" => "\n" * indent))
+    elseif x.data isa Matrix
+        fn = four_num(x.data, "")
+        nrow, ncol = size(x.data)
+        println("\nData: a $(nrow)×$(ncol) Matrix with contents as follows")
+        println("  minimum: ", fn[2])
+        println("  maximum: ", fn[4])
+        println("  number of missing values: ", fn[5])
+        println("  number of NaN values: ", fn[6])
+    else
+        println("\nData: a $(typeof(x.data)) object")
+    end
+end
+
 """
     summary(x::OA)
 
@@ -18,17 +41,7 @@ Print a summary of some of the contents of an OA object.
 function summarize(x::OA)
     println("OA Summary\n----------\n")
     println("Metadata: a Dict() with ", length(x.metadata), " entries")
-    # FIXME: what if data is a matrix, or a Dict ...?
-    if x.data isa DataFrame
-        println("\nData: a DataFrame with summary contents as follows")
-        df = DataFrame(name=String[], Min=Float64[], Max=Float64[], Mean=Float64[],
-            num_missing=Int64[], num_nan=Int64[])
-        for name in names(ctd.data)
-            push!(df, four_num(ctd[name], name))
-        end
-        indent = "  "
-        println(indent, replace(string(df), "\n" => "\n" * indent))
-    end
+    summarize_data(x)
 end
 
 """
@@ -48,17 +61,25 @@ summarize(d)
 function summarize(x::Ctd)
     println("CTD Summary\n-----------\n")
     println("Metadata: a Dict with ", length(x.metadata), " keys, including the following")
-    println("  filename:  ", x.metadata["filename"])
+    println("  filename:  \"", x.metadata["filename"], "\"")
     println("  latitude:  ", @sprintf "%.3fN" x.metadata["latitude"])
     println("  longitude: ", @sprintf "%.3fE" x.metadata["longitude"])
     println("  header:    String vector with ", length(x.metadata["header"]), " entries")
-    println("\nData: a DataFrame with summary contents as follows")
-    df = DataFrame(name=String[], Min=Float64[], Max=Float64[], Mean=Float64[],
-        num_missing=Int64[], num_nan=Int64[])
-    for name in names(x.data)
-        push!(df, four_num(x[name], name))
-    end
-    indent = "  "
-    println(indent, replace(string(df), "\n" => "\n" * indent))
+    summarize_data(x)
 end
+
+"""
+    summary(x::Topo)
+
+Print a summary of some of the contents of a Topography object.
+"""
+function summarize(x::Topography)
+    println("Topography Summary\n------------------\n")
+    println("Metadata: a Dict with ", length(x.metadata), " keys, including the following")
+    println("  filename: \"", x.metadata["filename"], "\"")
+    println("  latitude:  a vector of length ", length(x.metadata["latitude"]))
+    println("  longitude: a vector of length ", length(x.metadata["longitude"]))
+    summarize_data(x)
+end
+
 
