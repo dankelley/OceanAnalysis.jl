39ab43cef20a2c8b06e5b18d5352ebf3e7231c8c
dankelley <kelley.dan@gmail.com>
2026-02-28 11:38:57 -0400

    Improve N2(); see issue #71

--- src/seawater_properties.jl
+++ src/seawater_properties.jl
@@ -82,22 +82,24 @@ of sigma0 on pressure.
 
 In the present version, the spline is fitted with the `Dierckx::Spline1D()`
 function (Reference 1), which is provided with equal weights, `w`, for all
-points, with `k=3` to set the polynomial order to cubic, and with
-`bc="nearest"` to control what happens near boundaries. The user has no control
-over these things, although this might change in a future version of `N2()`.
+points, with `k=3` to set the polynomial order to cubic, and with the
+user-specified `bc` to control behaviour near top and bottom, along with `s` to
+control smoothness.  After the derivative is computed, any negative N² values
+are set to zero.
 
-The user's control rests in `s`, a smoothing parameter that is passed to
-`Dierckx:Spline1D()`. If not specified by the user, this defaults to a value
-that yields N² curves that are similar to those computed with a default call to
-`swN2()` in the `oce` R package.  Users may elect to use larger `s` values for
-smoother curves, or smaller ones to get more detail.  It would be a mistake not
-to pair experiments with `s` values with plots. As a start, it might be useful
-to examine Reference 2, which compares the R and Julia results.
+# Parameters
+
+- `ctd` a [Ctd] object
+
+# Keywords
+
+- `debug` an integer indicating whether to print information during processing. The default value of 0 means to work quietly, and any larger integer indicates to print some information.
+- `s` a numerical value, passed to `Dierckx::Spline1D()`, to control smoothing.
+- `bc` a string, passed to `Dierckx::Spline1D()`, that indicates what to do near boundaries. The default is `"nearest"`.
 
 # References
 
 1. https://github.com/JuliaMath/Dierckx.jl
-2. https://github.com/dankelley/OceanAnalysis.jl/issues/13
 
 # Examples
 
@@ -107,13 +109,12 @@ using OceanAnalysis, Plots
 pkgdir = dirname(dirname(pathof(OceanAnalysis)))
 filename = joinpath(pkgdir, "data", "ctd.cnv")
 ctd = read_ctd_cnv(filename);
-# Basic plot (left), profile-plot in oceanographic convention (right)
-p1=plot(N2(ctd), z_from_pressure.(ctd.data.pressure))
+p1=plot_profile(ctd, which="sigma0")
 p2=plot_profile(ctd, which="N2")
 plot(p1, p2)
 ```
 """
-function N2(ctd::Ctd, s::Float64=0.15; debug::Int64=0)
+function N2(ctd::Ctd; s::Float64=0.10, bc::String="nearest", debug::Int64=0)
     oad(debug, "N2([Ctd object]) START")
     pressure = ctd.data.pressure
     SA_ = SA(ctd)
@@ -122,15 +123,19 @@ function N2(ctd::Ctd, s::Float64=0.15; debug::Int64=0)
     i = sortperm(pressure)
     ok = diff(pressure[i]) .> 0.0
     ok = [ok[1]; ok]
-    oad(debug, "    ok length: $(length(ok))")
+    oad(debug, "    sum(ok): $(sum(ok)) before considering NaN sigma0")
+    ok = ok .& (0 .== isnan.(sigma0))
+    oad(debug, "    sum(ok): $(sum(ok)) after considering NaN sigma0")
     j = i[ok]
-    local spline = Spline1D(pressure[j], sigma0[j], w=ones(sum(ok)), k=3, bc="nearest", s=s)
+    #local spline = Spline1D(pressure[j], sigma0[j], w=ones(sum(ok)), k=3, bc="nearest", s=s)
+    local spline = Spline1D(pressure[j], sigma0[j], w=ones(sum(ok)), k=3, bc=bc, s=s)
     sigma0p = evaluate(spline, pressure)
     rho0 = 1000.0 + mean(sigma0p)
+    oad(debug, "    rho0: ", rho0)
     g = 9.8
     deriv = derivative(spline, pressure)
     N2 = (g / rho0) * deriv
-    oad(debug, "    N2 length: ", length(N2))
+    N2 = ifelse.(N2 .< 0.0, 0.0, N2)
     oad(debug, "END N2()")
     return N2
 end
