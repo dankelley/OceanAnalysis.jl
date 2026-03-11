0a86baa57d6223b0027ca362471409995a58d2fc
dankelley <kelley.dan@gmail.com>
2026-03-11 15:09:02 -0300

    read_nonna uses GMT (saving lots of fiddling)

--- src/nonna.jl
+++ src/nonna.jl
@@ -15,37 +15,23 @@ This returns a [`Nonna`](@ref) object that holds `metadata` and `data`. The `met
 
 ```julia
 using OceanAnalysis, Plots
+# Region near East Lawrencetown Beach Provincial Park
 n = read_nonna(expanduser("~/data/nonna/NONNA10_4460N06340W.tiff"));
-heatmap(n["northing"], n["easting"], n.data, color=:turbo,
+heatmap(n["longitude"], n["latitude"], n.data, color=:turbo,
     framestyle=:box, tickdirection=:out,
-    aspect_ratio=1.0, size=(800, 800),
-    xlab="Easting [m]", ylab="Northing [m]")
+    aspect_ratio=1.0, size=(800, 800))
+scatter!([], [], label=false, ms=3)
 ```
 """
-function read_nonna(filename::String, grid_size::Float64=10.0)
-    #url = "https://data.chs-shc.ca/dashboard/map"
-    f = "NONNA10_4460N06340W.tiff"
-    img = TiffImages.load(filename)
+function read_nonna(filename::String)
+    I = gmtread(filename)
     metadata = Dict()
-    # FIXME: put all tags into metadata? (Tricky, as interface is changeable.)
-    ifd = img.ifds[1]
+    metadata["longitude"] = I.x
+    metadata["latitude"] = I.y
+    data = I.z
     metadata["filename"] = filename
-    metadata["grid_size"] = grid_size
-    metadata["sample_time"] = ifd[TiffImages.DATETIME].data
-    tmp = ifd[TiffImages.MODELTIEPOINT].data
-    metadata["model_tie_point_lon"] = tmp[4]
-    metadata["model_tie_point_lat"] = tmp[5]
-    # Address missing-value codes (for land or bad data)
-    data = Float64.(img)
-    bad = data .> 1.0e38
-    data[bad] .= NaN
-    # Flip the y coordinates so that south is at the bottom of the image
-    data = reverse(data, dims=1)
-    s = size(data)
-    x = grid_size * range(0.0, length=s[1])
-    y = grid_size * range(0.0, length=s[2])
-    metadata["easting"] = x
-    metadata["northing"] = y
+    metadata["projection"] = I.proj4
+    metadata["inc"] = I.inc
     Nonna(metadata, data)
 end
 
--- src/setup.jl
+++ src/setup.jl
@@ -3,6 +3,7 @@ using Dates
 using DataFrames
 using Downloads
 using GibbsSeaWater
+using GMT
 using Interpolations
 using Plots
 using Printf
