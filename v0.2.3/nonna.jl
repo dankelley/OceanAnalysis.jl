93792ad74d8b8c39848fe21ae873f6c77396cf03
Documenter.jl <documenter@juliadocs.github.io>
2026-05-01 18:02:14 +0000

    build based on 9f0905e

---
+++ v0.2.3/nonna.jl
@@ -0,0 +1,6 @@
+using OceanAnalysis, Plots
+filename = expanduser("~/data/nonna/NONNA10_4460N06360W.tiff")
+n = read_nonna(filename);
+heatmap(n["longitude"], n["latitude"], n.data, c=:turbo,
+    size=(400, 400), dpi=300, framestyle=:box, tickdirection=:out)
+#savefig("nonna.png")
