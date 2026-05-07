3f0d4f0dbac6e4571b606a5e41e3bafb463bc3ee
Documenter.jl <documenter@juliadocs.github.io>
2026-05-07 15:49:43 +0000

    build based on 347ff85

---
+++ v0.2.4/topography.jl
@@ -0,0 +1,8 @@
+using OceanAnalysis, Plots, TiffImages
+topo_file = get_topography(-67, -63, 43, 46, resolution=1)
+topo = read_topography(topo_file);
+p1 = plot_topography(topo, domain=:both);
+p2 = plot_topography(topo, domain=:sea);
+p3 = plot_topography(topo, domain=:land);
+plot(p1, p2, p3, layout=(1, 3), size=(800, 200), dpi=300)
+savefig("topography.png")
