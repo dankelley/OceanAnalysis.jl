84f978fad0f61059bc27234ef2b18e9c55ee7698
dankelley <kelley.dan@gmail.com>
2026-05-24 14:23:51 -0300

    subset_dem() uses named lims (to allow for xlim etc)

--- docs/src/dem.jl
+++ docs/src/dem.jl
@@ -5,7 +5,7 @@ file = "/Users/kelley/Downloads/1044600063500_201901_DEM/1044600063500_201901_DE
 if isfile(file)
     dem_all = read_dem(file)
     # Focus near the Citadel fort
-    dem = subset_dem(dem_all, (-63.589, -63.572), (44.6426, 44.655))
+    dem = subset_dem(dem_all, lonlim=(-63.589, -63.572), latlim=(44.6426, 44.655))
     middle_lat = dem["latitude"][div(end + 1, 2)]
     aspect_ratio = 1.0 / cos(middle_lat * pi / 180.0)
     # Heatmap of elevation
