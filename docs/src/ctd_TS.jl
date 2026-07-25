2879243613cf54244951a248228a15cccdd4e396
dankelley <kelley.dan@gmail.com>
2026-07-25 09:53:51 -0300

    Does this fix broken remote build

--- docs/src/ctd_TS.jl
+++ docs/src/ctd_TS.jl
@@ -1,9 +1,7 @@
 # Read and plot a built-in CTD file
 using OceanAnalysis, Dates, Measures, Plots, Printf
-filename = joinpath(dirname(dirname(pathof(OceanAnalysis))),
-    "data", "ctd.cnv")
+filename = joinpath(dirname(dirname(pathof(OceanAnalysis))), "data", "ctd.cnv")
 ctd = read_ctd_cnv(filename);
-plot_TS(ctd, ms=3, markerstrokewidth=0, color_by="pressure");
-title = @sprintf("CTD observations at %.3fN and %.3fE on %s",
-    ctd["latitude"], ctd["longitude"], ctd["time"])
+title = @sprintf "CTD observations at %.3fN and %.3fE on %s" ctd["latitude"] ctd["longitude"] ctd["time"]
+plot_TS(ctd, ms=3, title=title, markerstrokewidth=0, color_by="pressure")
 savefig("ctd_TS.png")
