b2ab2a0f44425bee27298604624c56c4a271f7a6
Documenter.jl <documenter@juliadocs.github.io>
2026-08-05 14:40:31 +0000

    build based on f129bf5

---
+++ v0.2.12/amsr.jl
@@ -0,0 +1,8 @@
+# North Atlantic Sea Surface Temperature
+using OceanAnalysis, Plots, Dates
+f = get_amsr("2025-09-07");
+a = read_amsr(f, "SST");
+plot_amsr(a, xlims=(290.0, 340.0), ylims=(30.0, 60.0),
+    draw_contours=0.0:2.5:30.0, clim=(0, 30))
+savefig("amsr.png")
+
