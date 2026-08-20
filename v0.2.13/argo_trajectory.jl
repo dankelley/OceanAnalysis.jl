fed63dfb8a1ea199c172349ec2f1a3c4a4ef3a37
Documenter.jl <documenter@juliadocs.github.io>
2026-08-20 15:13:02 +0000

    build based on 018804c

---
+++ v0.2.13/argo_trajectory.jl
@@ -0,0 +1,26 @@
+# Plot a float trajectory with colour for sequence number
+using OceanAnalysis, Plots, Printf, Statistics
+ID = r"D4902911" # focus on this ID
+index_file = get_argo_index("~/data/argo");
+index_all = read_argo_index(index_file) # 3.2e6 profiles
+index = index_all[occursin.(ID, index_all.file), :]
+sort!(index, :time) # this lets us join dots in time order
+lon, lat = index.longitude, index.latitude
+plot(lon, lat,
+    aspect_ratio=1.0 / cos(mean(lat) * pi / 180),
+    framestyle=:box, color=:gray, dpi=200,
+    title=@sprintf("Argo float %s coloured by cycle index", ID.pattern),
+    titlefontsize=9)
+colors = cgrad(:turbo)
+scatter!(lon, lat, marker_z=1:length(lon),
+    markersize=3, markerstyle=:circle, color=colors)
+# Add land and 1km isobath
+plot_coastline!(coastline())
+topo_file = get_topography(-110.0, -30, 20, 60, resolution=30,
+    destdir="~/data/topo")
+topo = read_topography(topo_file)
+contour!(topo.metadata["longitude"], topo.metadata["latitude"],
+    topo.data, xlim=xlims(), ylim=ylims(),
+    color=:gray, linewidth=2, colorbar_entry=false, levels=[-1000.0])
+scale_bar(500; x=:right, y=:top)
+savefig("argo_trajectory.png")
