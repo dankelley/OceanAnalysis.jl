9f07346b3868a570aba4c002ff084b4c283c30b1
dankelley <kelley.dan@gmail.com>
2026-09-06 15:10:25 -0300

    plot_TS() now plots sigma0 and spiciness0 contours

--- src/plot_TS.jl
+++ src/plot_TS.jl
@@ -1,21 +1,24 @@
 using GibbsSeaWater: gsw_ct_freezing, gsw_ct_from_t, gsw_sa_from_sp, gsw_sigma0, gsw_spiciness0
 
 """
-    plot_freezing_curve!(xlim, ylim; kwargs...)
+    plot_freezing_curve(ax; color=:darkgray, linewidth=1, n=50, debug=0)
 
 Draw a freezing-point curve on an existing CT-SA plot. This is called by
 [`plot_TS`](@ref), but can also be called by the user, if customization of line
-type, etc, is required.
+color and width is required.
 """
-function plot_freezing_curve!(; kwargs...) # BROKEN -- uses Plots.jl
-    n = 50 # it is a pretty straight curve
-    xlim = xlims()
-    ylim = ylims()
-    SA = range(xlim[1], xlim[2], length=n)
+function plot_freezing_curve(ax; color=:darkgray, linewidth=1.8, n=50, debug=0)
+    oad(debug, "plot_freezing_curve() START")
+    axlims = ax.finallimits[]
+    SAmin = minimum(axlims)[1]
+    SAmax = maximum(axlims)[1]
+    oad(debug, "    SAmin=$SAmin, SAmax=$SAmax")
+    SA = range(SAmin, SAmax, length=n)
     CT = gsw_ct_freezing.(SA, 0.0, 1.0) # SA, p, saturation_fraction
-    plot!(SA, CT, label=false, color=:darkgray, xlim=xlim, ylim=ylim; kwargs...)
+    lines!(ax, SA, CT, color=color, linewidth=linewidth)
+    oad(debug, "END plot_freezing_curve()")
 end
-export plot_freezing_curve!
+export plot_freezing_curve
 
 
 """
@@ -28,9 +31,9 @@ Plot an oceanographic TS diagram, with the Gibbs Seawater equation of state.
 Whether contours of density and spiciness are drawn depends on values of the
 `sigma0_levels` and `spiciness0_levels`. By default, a freezing-point line is
 drawn (if it is within the range of the data) by calling
-[`plot_freezing_curve!`](@ref). If customization of line width, color, etc., is
+[`plot_freezing_curve`](@ref). If customization of line width, color, etc., is
 required, uses `plot_freezing=false` and then call
-[`plot_freezing_curve!`](@ref) directly.
+[`plot_freezing_curve`](@ref) directly.
 
 By default, axis names are written in long form; set `abbreviate=true` for
 shorter versions.
@@ -117,17 +120,13 @@ plot_TS(ctd, title="Built-in CTD file")
 # Example 2: just symbols, with no line.
 plot_TS(ctd, seriestype=:scatter)
 
-# Example 3: some customization
+# Example 3: some customizations
 plot_TS(ctd, color=:red, debug=1, markercolor=:blue)
 plot_TS(ctd, seriestype=:scatter, markersize=3.0, color=:blue)
 plot_TS(ctd, seriestype=:lines, linewidth=0.7)
 
-#<FIXME:> # Example 4: color_by pressure.
-#<FIXME:> # The markers are drawn without borders, to avoid black overpainting.
-#<FIXME:> plot_TS(ctd, markerstrokewidth=0, markersize=3, color_by="pressure")
-#<FIXME:> 
-#<FIXME:> # Example 5: black/white plot, but with space where a palette would go.
-#<FIXME:> plot_TS(ctd, color_by="")
+# Example 4: colour dots by pressure
+plot_TS(ctd; seriestype=:scatter, markersize=6, color_by="pressure")
 ```
 """
 function plot_TS(d; sigma0_levels=[], spiciness0_levels=0,
@@ -142,9 +141,9 @@ function plot_TS(d; sigma0_levels=[], spiciness0_levels=0,
     else
         error("plot_TS() only works on Argo and Ctd objects")
     end
-    oad(debug, "  sigma0_levels: $sigma0_levels")
-    oad(debug, "  spiciness0_levels: $spiciness0_levels")
-    oad(debug, "  plot_freezing: $plot_freezing")
+    oad(debug, "    sigma0_levels: $sigma0_levels")
+    oad(debug, "    spiciness0_levels: $spiciness0_levels")
+    oad(debug, "    plot_freezing: $plot_freezing")
     local S = d.data.salinity
     local T = d.data.temperature
     local p = d.data.pressure
@@ -157,22 +156,22 @@ function plot_TS(d; sigma0_levels=[], spiciness0_levels=0,
         @warn "plot_TS(): no good SA,CT pairs, so plotting an aphysical default"
     end
     # Draw the data.
-    oad(debug, "  drawing the data")
+    oad(debug, "    drawing the data")
     using_color_by = false
-    if color_by != false
+    if color_by !== false
         if isa(color_by, String)
-            oad(debug, "  color_by: \"", color_by, "\"")
+            oad(debug, "    color_by: \"", color_by, "\"")
             if color_by in names(d.data)
                 color_by = decode_color_by(d[color_by])
-                oad(debug, "    decoded palette details with decode_color_by()")
+                oad(debug, "    ... decoded palette details with decode_color_by()")
                 cindex = (color_by.levels .- color_by.clims[1]) / (color_by.clims[2] - color_by.clims[1])
-                oad(debug, "    computed cindex")
+                oad(debug, "    ... computed cindex")
                 colormap = cgrad(color_by.colorscheme)
-                oad(debug, "    computed colormap")
+                oad(debug, "    ... computed colormap")
                 color = colormap[cindex]
-                oad(debug, "    computed color")
+                oad(debug, "    ... computed color")
             elseif color_by == ""
-                oad(debug, "  no palette will be drawn, since color_by=\"\"")
+                oad(debug, "    no palette will be drawn, since color_by=\"\"")
             else
                 error("color_by is \"", color_by, "\" which is neither \"\" nor in names(d.data)")
             end
@@ -186,10 +185,10 @@ function plot_TS(d; sigma0_levels=[], spiciness0_levels=0,
         using_color_by = true
     end
     if using_color_by
-        oad(debug, "  set up color_by vector")
+        oad(debug, "    set up color_by vector")
     end
     kwargs_dict = Dict{Symbol,Any}(kwargs)
-    oad(debug, "  keys in kwargs_dict: $(collect(keys(kwargs_dict)))")
+    oad(debug, "    keys in kwargs_dict: $(collect(keys(kwargs_dict)))")
     title = pop!(kwargs_dict, :title, "")
     xlabel = abbreviate ? "SA [g/kg]" : "Absolute Salinity [g/kg]"
     ylabel = abbreviate ? "CT [°C]" : "Conservative Temperature [°C]"
@@ -204,103 +203,56 @@ function plot_TS(d; sigma0_levels=[], spiciness0_levels=0,
     ylims = pop!(kwargs_dict, :ylims, extend_extrema(CT))
     limits!(ax, xlims[1], xlims[2], ylims[1], ylims[2])
     linewidth = pop!(kwargs_dict, :linewidth, 1.0)
-    oad(debug, "  set linewidth=$linewidth")
+    oad(debug, "    set linewidth=$linewidth")
+    colormap = pop!(kwargs_dict, :colormap, :turbo)
     if using_color_by
-        oad(debug, "  color being set by color_by")
+        oad(debug, "    will use colormap :$colormap for color_by")
     else
         color = pop!(kwargs_dict, :color, :black)
-        oad(debug, "  set color=$color")
+        oad(debug, "    set color=$color")
     end
     marker = pop!(kwargs_dict, :marker, :circle)
-    oad(debug, "  set marker=$marker")
+    oad(debug, "    set marker=$marker")
     markercolor = pop!(kwargs_dict, :markercolor, :black)
-    oad(debug, "  set markercolor=$markercolor")
+    oad(debug, "    set markercolor=$markercolor")
     markersize = pop!(kwargs_dict, :markersize, 5.0)
-    oad(debug, "  set markersize=$markersize")
+    oad(debug, "    set markersize=$markersize")
     seriestype = pop!(kwargs_dict, :seriestype, :scatterlines)
-    oad(debug, "  set seriestype=$seriestype")
+    oad(debug, "    set seriestype=$seriestype")
     seriestype in (:lines, :scatter, :scatterlines) || error("seriestype is '$seriestype', but it must be :line, :scatter or :scatterline")
-    #<disabled>            oad(debug, "  plotting symbols with individual colours")
-    #<disabled>            cindex = (color_by.levels .- color_by.clims[1]) / (color_by.clims[2] - color_by.clims[1])
-    #<disabled>            colormap = cgrad(color_by.colorscheme)
-    #<disabled>            markercolor = colormap[cindex]
-
     if seriestype == :lines
-        oad(debug, "  calling lines!()")
+        oad(debug, "    calling lines!()")
         lines!(ax, SA, CT, color=color, linewidth=linewidth)
     elseif seriestype == :scatter
-        oad(debug, "  calling scatter!()")
+        oad(debug, "    calling scatter!()")
         scatter!(ax, SA, CT, marker=marker, markersize=markersize, color=color)
     elseif seriestype == :scatterlines
-        oad(debug, "  calling scatterlines!()")
-        # FIXME: handle colorby
+        oad(debug, "    calling scatterlines!()")
         scatterlines!(ax, SA, CT, marker=marker, markersize=markersize, markercolor=markercolor,
             linewidth=linewidth, color=color)
     else
         error("seriestype=$seriestype not permitted; try :lines, :scatter or :scatterlines")
     end
     if plot_freezing
-        SAf = range(xlims[1], xlims[2], length=100)
-        CTf = gsw_ct_freezing.(SAf, 0.0, 1.0) # SA, p, saturation_fraction
-        lines!(SAf, CTf, color=:darkgray)
+        plot_freezing_curve(ax; debug=increment_debug(debug))
     end
-
-    println("FIXME: draw color_by palette (top priority)")
-    println("FIXME: document color_by")
-    println("FIXME: document xlims and ylims (IMPORTANT: lim or lims? what is the wording)")
-    println("FIXME: add isopycnal contours")
-    println("FIXME: add spiciness contours")
-    println("FIXME: does scatterlines obey all relevant kwargs?")
+    if using_color_by
+        oad(debug, "    drawing colorbar")
+        Colorbar(fig[1, 2], colormap=colormap, limits=color_by.clims, ticklabelsize=fontsize)
+    end
+    plot_TS_sigma0_contours(ax; levels=sigma0_levels, debug=increment_debug(debug))
+    plot_TS_spiciness0_contours(ax; levels=spiciness0_levels, debug=increment_debug(debug))
+    println("FIXME: make scatterlines handle color_by (etc - lots to do)")
+    oad(debug, "END plot_TS()")
     return fig
-    #<disabled>    p_TS = plot(SA, CT,
-    #<disabled>        xlabel=abbreviate ? "SA [g/kg]" : "Absolute Salinity [g/kg]",
-    #<disabled>        ylabel=abbreviate ? "CT [°C]" : "Conservative Temperature [°C]",
-    #<disabled>        yrot=90,
-    #<disabled>        framestyle=:box, legend=false, color=color, tickdirection=:out,
-    #<disabled>        seriestype=:path, linewidth=1.0, marker=:circle, markersize=1.4,
-    #<disabled>        tickfontsize=fontsize, guidefontsize=fontsize, titlefontsize=fontsize;
-    #<disabled>        kwargs...)
-    #<disabled>    # Possibly add freezing-point curve
-    #<disabled>    if plot_freezing
-    #<disabled>        plot_freezing_curve!()
-    #<disabled>    end
-    #<disabled>    # Possibly add density contours
-    #<disabled>    plot_TS_sigma0_contours(sigma0_levels; debug=debug)
-    #<disabled>    plot_TS_spiciness0_contours(spiciness0_levels; debug=debug)
-    #<disabled>    # Redraw the data, so they appear above other elements such as 
-    #<disabled>    # contours and the freezing-point line. Note that the path will be
-    #<disabled>    # drawn with the provided the 'color'.
-    #<disabled>    if using_color_by
-    #<disabled>        if color_by == ""
-    #<disabled>            oad(debug, "  not plotting symbols with individual colours, but leaving palette space")
-    #<disabled>            p_cbar = plot(ticks=nothing, border=:none)
-    #<disabled>            l = grid(1, 2, widths=[0.88, 0.12])
-    #<disabled>            p_TS = plot(p_TS, p_cbar, layout=l)
-    #<disabled>        else
-    #<disabled>            oad(debug, "  plotting symbols with individual colours")
-    #<disabled>            cindex = (color_by.levels .- color_by.clims[1]) / (color_by.clims[2] - color_by.clims[1])
-    #<disabled>            colormap = cgrad(color_by.colorscheme)
-    #<disabled>            markercolor = colormap[cindex]
-    #<disabled>            plot!(SA, CT, seriestype=:scatter,
-    #<disabled>                legend=false, linecolor=color, markercolor=markercolor,
-    #<disabled>                linewidth=1.0, marker=:circle, markersize=1.4;
-    #<disabled>                kwargs...)
-    #<disabled>            p_cbar = scatter([1], [NaN], zcolor=[color_by.clims[1]], colormap=colormap, clims=color_by.clims, cbar=true, ticks=false, framestyle=:none, label="")
-    #<disabled>            l = grid(1, 2, widths=[0.88, 0.12])
-    #<disabled>            p_TS = plot(p_TS, p_cbar, layout=l)
-    #<disabled>        end
-    #<disabled>    end
-    #<disabled>    oad(debug, "END plot_TS()")
-    #<disabled>    return p_TS
 end
 export plot_TS
 
 
 
 """
-    plot_TS_sigma0_contours(levels=[];
-        color=:gray50, linewidth=1.19*default(:gridlinewidth),
-        debug::Integer=0)
+    plot_TS_sigma0_contours(ax; levels=[],
+        color=:gray75, linewidth=2.0, debug::Integer=0)
 
 Add contours of density to an existing TS plot.  This is used by
 [`plot_TS`](@ref), but can also be used separately, if the TS data
@@ -325,16 +277,19 @@ have been drawn by other means.
    processing.
 
 """
-function plot_TS_sigma0_contours(levels=[];
-    color=:gray50, linewidth=1.19 * default(:gridlinewidth),
-    debug::Integer=0)
+function plot_TS_sigma0_contours(ax; levels=[],
+    color=:gray75, alpha=0.5, linewidth=2.0, linestyle=:solid, debug::Integer=0)
     oad(debug, "plot_TS_sigma0_contours() START")
     oad(debug, "  levels: ", levels)
-    xlim = xlims()
-    ylim = ylims()
-    SAc = range(xlim[1], xlim[2], length=300)
-    CTc = range(ylim[1], ylim[2], length=300)
-    sigma0c = gsw_sigma0.(SAc', CTc) |> fix_gsw_bad_code!
+    axlims = ax.finallimits[]
+    SAmin = minimum(axlims)[1]
+    SAmax = maximum(axlims)[1]
+    CTmin = minimum(axlims)[2]
+    CTmax = maximum(axlims)[2]
+    oad(debug, "    SAmin=$SAmin, SAmax=$SAmax, CTmin=$CTmin, CTmax=$CTmax")
+    SAc = range(SAmin, SAmax, length=300)
+    CTc = range(CTmin, CTmax, length=300)
+    sigma0c = gsw_sigma0.(SAc, CTc') |> fix_gsw_bad_code!
     if length(levels) == 0
         oad(debug, "  case 1: levels is empty, so auto-compute sigma0 contour levels")
         levels = pretty(sigma0c) # returns [] if min=max
@@ -350,9 +305,9 @@ function plot_TS_sigma0_contours(levels=[];
         oad(debug, "  case 3: levels is a vector of sigma0 levels for contouring")
     end
     if length(levels) > 0
-        contour!(SAc, CTc, sigma0c, xlim=xlim, ylim=ylim, levels=levels,
-            linewidth=linewidth, color=color, cbar=false, clabels=true,
-            foreground_color_axis=:black, foreground_color_border=:black)
+        oad(debug, "    about to contour sigma0")
+        contour!(ax, SAc, CTc, sigma0c, levels=levels, labels=true,
+            linewidth=linewidth, linestyle=linestyle, color=color, alpha=alpha)
     end
     oad(debug, "END plot_TS_sigma0_contours")
 end
@@ -360,9 +315,8 @@ export plot_TS_sigma0_contours
 
 
 """
-    plot_TS_spiciness0_contours(levels=[];
-        color=:gray50, linewidth=1.19*default(:gridlinewidth),
-        debug::Integer=0)
+    plot_TS_spiciness0_contours(ax; levels=[],
+        color=:gray75, linewidth=2.0, debug::Integer=0)
 
 Add contours of spiciness0 to an existing TS plot.  This is used by
 [`plot_TS`](@ref), but can also be used separately, if the TS data
@@ -370,19 +324,22 @@ have been drawn by other means.  For the meanings of the
 arguments and keywords, see the documentation for
 [`plot_TS_sigma0_contours`](@ref).
 """
-function plot_TS_spiciness0_contours(levels=[];
-    color=:gray50, linewidth=1.19 * default(:gridlinewidth),
-    debug::Integer=0)
+function plot_TS_spiciness0_contours(ax; levels=[],
+    color=:gray75, alpha=0.5, linewidth=2.0, linestyle=(:dot, :dense), debug::Integer=0)
     oad(debug, "plot_TS_spiciness0_contours() START")
     oad(debug, "  levels: ", levels)
-    xlim = xlims()
-    ylim = ylims()
-    SAc = range(xlim[1], xlim[2], length=300)
-    CTc = range(ylim[1], ylim[2], length=300)
-    spiciness0c = gsw_spiciness0.(SAc', CTc) |> fix_gsw_bad_code!
+    axlims = ax.finallimits[]
+    SAmin = minimum(axlims)[1]
+    SAmax = maximum(axlims)[1]
+    CTmin = minimum(axlims)[2]
+    CTmax = maximum(axlims)[2]
+    oad(debug, "    SAmin=$SAmin, SAmax=$SAmax, CTmin=$CTmin, CTmax=$CTmax")
+    SAc = range(SAmin, SAmax, length=100)
+    CTc = range(CTmin, CTmax, length=300)
+    spiciness0c = gsw_spiciness0.(SAc, CTc') |> fix_gsw_bad_code!
     if length(levels) == 0
         oad(debug, "  case 1: spiciness0_levels is empty, so auto-compute spiciness0 contour levels")
-        spiciness0_levels = pretty(spiciness0c) # returns [] if min=max
+        levels = pretty(spiciness0c) # returns [] if min=max
     elseif length(levels) == 1 && isa(levels, Integer)
         if levels > 0
             oad(debug, "  case 2a: auto-selecting $levels spiciness0 levels to contour")
@@ -395,11 +352,9 @@ function plot_TS_spiciness0_contours(levels=[];
         oad(debug, "  case 3: levels is a vector of spiciness0 levels for contouring")
     end
     if length(levels) > 0
-        oad(debug, "  drawing spiciness0 contours at levels $(levels)")
-        contour!(SAc, CTc, spiciness0c, xlim=xlim, ylim=ylim,
-            linewidth=contour_linewidth, color=:gray50,
-            levels=levels, cbar=false, clabels=true,
-            foreground_color_axis=:black, foreground_color_border=:black)
+        oad(debug, "    about to contour spiciness0")
+        contour!(ax, SAc, CTc, spiciness0c, levels=levels, labels=true,
+            linewidth=linewidth, linestyle=linestyle, color=color, alpha=alpha)
     end
     oad(debug, "END plot_TS_spiciness0_contours")
 end
