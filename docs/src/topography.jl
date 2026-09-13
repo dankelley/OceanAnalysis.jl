6b4e61a723c15ae490bf3ddb946cf23c8d53ce8b
dankelley <kelley.dan@gmail.com>
2026-09-13 12:57:59 -0300

    Update topography webpage BUT land/sea not coloured

--- docs/src/topography.jl
+++ docs/src/topography.jl
@@ -1,9 +1,14 @@
-using OceanAnalysis, Plots
+using OceanAnalysis, GLMakie
 topo_file = get_topography(-67, -63, 43, 46, resolution=1)
 topo = read_topography(topo_file);
-fs = 7
-p1 = plot_topography(topo, domain=:both, fontsize=fs);
-p2 = plot_topography(topo, domain=:sea, fontsize=fs);
-p3 = plot_topography(topo, domain=:land, fontsize=fs);
-plot(p1, p2, p3, layout=(1, 3), size=(800, 200), dpi=200)
-savefig("topography.png")
+
+# Single panel
+fig = plot_topography(topo) # domain defaults to :land_and_sea
+save("topography_1.png", fig)
+
+# Multiple panels
+fig = Figure(size=(800, 200))
+plot_topography!(fig[1, 1], topo, domain=:land_and_sea) # same as above
+plot_topography!(fig[1, 2], topo, domain=:sea)
+plot_topography!(fig[1, 3], topo, domain=:land)
+save("topography_2.png", fig)
